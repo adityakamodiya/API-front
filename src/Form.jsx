19707a214968f73ec55c3b9f6a8b4d6369a0000b
Aditya Kamodiya <adityakamodiya@gmail.com>
2024-07-23 19:05:48 +0530

    Update Form.jsx

--- src/Form.jsx
+++ src/Form.jsx
@@ -5,7 +5,7 @@ function form() {
     const [location,setlocation] = useState('')
     const [price,setprice] = useState('')
     const [descripition,setdescripition] = useState('')
-    const [workingdays,setworkingdays] = useState('')
+    const [Fees,setFees] = useState('')
     const [timings,settimings] = useState('')
     const[images,setimages] = useState('')
 
@@ -15,7 +15,7 @@ function form() {
         formData.append('Name', name);
         formData.append('location', location);
         formData.append('price', price);
-        formData.append('working_days', workingdays);
+        formData.append('Fees', Fees);
         formData.append('timings', timings);
         formData.append('file', file);
         // formData.append('Message', cityName);
@@ -27,7 +27,7 @@ function form() {
     <input type="text"placeholder='name' value={name} onChange={(e)=>{setname(e.target.value)}} />
     <textarea name="" value={location}placeholder='location' id="" onChange={e=>setlocation(location)}></textarea>
     <input type="number" value={price} placeholder='price' onChange={e=>setprice(e.target.value)} />
-    <input type="text" value={workingdays} placeholder='working days' onChange={e=>setworkingdays(e.target.value)} />
+    <input type="text" value={workingdays} placeholder='working days' onChange={e=>Fees(e.target.value)} />
     <input type="text" value={timings} placeholder='timings' onChange={e=>settimings(e.target.value)} />
     <input type="file" value={images} placeholder='file' onChange={e=>e.target.files[0]} />
     <button type='submit'>submit</button>
