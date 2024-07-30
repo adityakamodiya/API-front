4e557d6b5a0477a718e0cbb09b0ce019970b315c
Aditya Kamodiya <adityakamodiya@gmail.com>
2024-07-30 19:38:08 +0530

    ...

--- src/GetAll.jsx
+++ src/GetAll.jsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react'
   import axios from 'axios';
 function GetAll() {
-<<<<<<< HEAD
     const[no,set] = useState(5);
     axios.get(`https://server-5a5x.onrender.com/`)
-=======
-    axios.get('https://server-5a5x.onrender.com/')
->>>>>>> 3e5f2134c36c246051648c435e788b0c99ba760a
+    // axios.get('https://server-5a5x.onrender.com/')
     .then((res)=>console.log(res))
 
   return (
