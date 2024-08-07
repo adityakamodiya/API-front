705c57035fd2813f67de1e25bec7071f546f8e35
Aditya Kamodiya <adityakamodiya@gmail.com>
2024-08-07 23:04:43 +0530

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       <div>
         <section>                              
           <Routes>
-            <Route path="/form" element={<Form />} />
+          <Route path="/form" element={ud ? <Form /> : <Navigate to="/" />} />
             <Route path="/signup" element={<Register />} />
             <Route 
               path="/" 
