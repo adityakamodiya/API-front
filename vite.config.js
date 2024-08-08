83f846a5619e27c5fdac5e3168102ee2a905d0c6
Aditya Kamodiya <adityakamodiya@gmail.com>
2024-08-08 15:40:52 +0530

    ...

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Make sure this is correct for your deployment
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true,
+  },
 });
