10193cbd3e1cfc6db8e7cc6b0cd25d1db49313e8
Aditya Kamodiya <adityakamodiya@gmail.com>
2024-08-08 11:55:48 +0530

    ...

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
- 
-})
+  base: '/', // Ensure this is set correctly based on your deployment
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
+});
