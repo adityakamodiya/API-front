cc46920449f43bf96890c14f2c18ec8ec4ed792c
Aditya Kamodiya <adityakamodiya@gmail.com>
2024-08-08 11:28:46 +0530

    ...

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -1,13 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {getAuth} from 'firebase/auth'
-import {getFirestore} from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from 'firebase/analytics';
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBN8dneTw9pHaN9X05g_Z7SpXovB0-73b0",
   authDomain: "apiform-b3bbc.firebaseapp.com",
@@ -20,9 +17,15 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const  auth = getAuth();
-export default app;
-
-export const db = getFirestore(app); 
+const auth = getAuth(app);
+const db = getFirestore(app);
 const analytics = getAnalytics(app);
-auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL);
+
+// Set persistence for Firebase Auth
+setPersistence(auth, browserLocalPersistence)
+  .catch((error) => {
+    console.error("Error setting persistence:", error);
+  });
+
+export { auth, db };
+export default app;
